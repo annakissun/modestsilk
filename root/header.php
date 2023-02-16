00897081baf9f373172eda007a9decee40dc27aa
annakissun <125438648+annakissun@users.noreply.github.com>
2023-02-16 14:05:02 +0800

    bla bla bla

---
+++ root/header.php
@@ -0,0 +1,37 @@
+<html>
+<head><title>Modest Silk</title>
+<style>
+*  {
+	box-sizing: border-box;
+}
+body {
+	background: #DDC1BD;
+	font-family: Courier New;
+	padding: 10px;
+}
+#header {
+	padding: 10px;
+	text-align: center;
+	color: #FFFFFF;
+	background: #DDC1BD;
+}
+h1 {
+	font-size: 50px;
+	font-weight: bold;
+}
+h2 {
+	font-weight: bold;
+	font-style: italic;
+}
+</style>
+</head>
+<body>
+
+<div id="header">
+<h1>Modest Silk</h1>
+<!--- motto --->
+<p><h2> Your First Choice.</h2></p>
+</div>
+
+</body>
+</html>
---
+++ root/main.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<style>
+#main {
+    background-color: #DDC1BD;
+    padding: 15px;
+}
+/*warna table mesti sama dengan warna latar poster*/
+table {
+    background-color: #008B2D; /*warna table*/
+}
+td {
+    width: 30%;
+}
+tr {
+    height: 50px;
+}
+tr:nth-child(3) {
+    text-align: center; /*kedudukan butang*/
+}
+/* CSS untuk butang */
+.btn {
+  -webkit-border-radius: 10;
+  -moz-border-radius: 10;
+  border-radius: 10px;
+  font-family: Courier New;
+  color: #000000;
+  font-size: 20px;
+  background: #f5f5f5;
+  padding: 10px 20px 10px 20px;
+  border: solid #bd5e7e 2px;
+  text-decoration: none;
+}
+
+.btn:hover {
+  background: #bd5e7e;
+  text-decoration: none;
+}
+</style>
+</head>
+</html>
